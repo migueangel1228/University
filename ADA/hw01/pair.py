52eff1dd284215d159b3ac971e6f98be159a8475
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-02-17 13:43:34 -0500

    Create pair.py

---
+++ ADA/hw01/pair.py
@@ -0,0 +1,35 @@
+
+"""
+Tarea  : ADA  Tarea 1 
+Fecha  : 14 Febrero 2026
+Nombre : Miguel Angel Padilla Rosero
+Cod    : 8988878
+
+Problem C - N + NOD(N)
+"""
+
+
+from sys import stdin
+
+def main():
+    x = x
+    
+main()
+
+"""
+Sample Input
+3
+0 0
+10000 10000
+20000 20000
+5
+0 2
+6 67
+43 71
+39 107
+189 140
+0
+Sample Output
+INFINITY
+36.2215
+"""
