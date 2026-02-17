be4e94f446c784979790882d338de60e8ad36316
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-02-17 13:14:45 -0500

    Implement NOD sequence logic and main
    Remove obsolete README; fully rewrite ADA/hw01/nod.py to implement the NOD-based sequence solution: add MAX limit, correct binary search, compute smallest-prime-factor sieve, build NOD (divisor-count) array, generate the sequence N_i = N_{i-1} + NOD(N_{i-1}) and add a main() that answers cases using binary search. Replace poposito.txt contents with precomputed case outputs (replacing large input blob with expected results). Move examplesDivide&Conquest.pdf into ADA/notes (rename only). These changes implement the algorithm, fix bugs (integer division, indexing), and add I/O formatting for contest-style output.

--- ADA/hw01/nod.py
+++ ADA/hw01/nod.py
@@ -1,3 +1,4 @@
+
 """
 Tarea  : ADA  Tarea 1 
 Fecha  : 14 Febrero 2026
@@ -6,58 +7,99 @@ Cod    : 8988878
 
 Problem C - N + NOD(N)
 """
+
 from sys import stdin
-from math import pow
 
-#Binary Search
+MAX = 1000010  # Dado que se tiene que incluir el 1e6 + 1, es correcto
 
-def binarySearch(A,valueLow,valueHigh):
-    ans = 0
-    lowIndx , hiIndx = -1 ,-1 
-    lowAux , hiAux = 0,len(A) 
-    while ( hiAux - lowAux > 1):
-        mid = lowAux + (hiAux - lowAux) / 2;
-
+def binarySearch(A, valueLow, valueHigh):
+    # ultimo indice con A[i] <= valueLow
+    lowAux, hiAux = -1, len(A)
+    while hiAux - lowAux > 1:
+        mid = lowAux + (hiAux - lowAux) // 2
         if A[mid] <= valueLow:
             lowAux = mid
         else:
             hiAux = mid
-    lowIndx  = lowAux
-    
-    lowAux , hiAux = lowIndx, len(A) 
-    while ( hiAux - lowAux > 1):
-        mid = lowAux + (hiAux - lowAux) / 2;
+    lowIndx = lowAux  
 
+    # primer indice con A[i] >= valueHigh
+    lowAux, hiAux = lowIndx, len(A)
+    while hiAux - lowAux > 1:
+        mid = lowAux + (hiAux - lowAux) // 2
         if A[mid] < valueHigh:
             lowAux = mid
         else:
             hiAux = mid
-    hiIndx = hiAux
-    
-    ans = hiIndx - lowIndx  
-        
-    return ans
+    hiIndx = hiAux  
 
-def crearSusecion(Nod):
-    N = []
-    for i in range(1e6 - 1):
-        if i == 0:
-            N[i] = 1
-        else:
-            N[i] = Nod[i - 1] + i - 1
+    return hiIndx - lowIndx - 1
+
+
+def sucesion(Nod):
+    # N_0 = 1; 
+    # N_i = N_i-1 + NOD(N_i-1)
+    N = [1]
+    while N[-1] < MAX:
+        anterior = N[-1]
+        sigueinte = anterior + Nod[anterior]
+        N.append(sigueinte)
     return N
 
 
-def Nod():
-    x = x
+# Encuentra el exponente de a en n tal que, n = a^b * c
+def numExponent(n, a):
+    ans = 0
+    if n % a == 0:
+        ans = 1 + numExponent(n // a, a) # se le agrega 1 a b
+    return ans
+
+
+def makeNodArray(criba, div):
+    nodeArray = [0 for _ in range(MAX)]
+    nodeArray[1] = 1
+    for n in range(2, MAX):
+        if criba[n]:
+            nodeArray[n] = 2
+        else:
+            p = div[n]
+            b = numExponent(n, p)
+            nodeArray[n] = (b + 1) * nodeArray[n // (p ** b)] 
+    return nodeArray
+
+
+#Se quiere encontrar maximo exponente del divisor primo, a de n
+def calculateMaxPrimeFactor():
+    criba = [True for _ in range(MAX)]
+    div = [None for _ in range(MAX)]
+    criba[0] = criba[1] = False
+
+    for j in range(4, MAX, 2):
+        criba[j], div[j] = False, 2
+
+    for i in range(3, MAX, 2):
+        if criba[i]:
+            for j in range(i * i, MAX, i):
+                criba[j], div[j] = False, i
+    return criba, div
 
 
 def main():
-    n = int(stdin.readline())
-    for casito in range(n):
-        low , high = list(map(int,stdin.readline.split()))
-        
-        
+    # precalculos (una sola vez)
+    criba, div = calculateMaxPrimeFactor()
+    nod = makeNodArray(criba, div)
+    seqN = sucesion(nod)
+
+    totalCases = int(stdin.readline())
+    for casito in range(totalCases):
+        low, high = map(int, stdin.readline().split())
+
+        # binarySearch incluye al low, yal high, usamos (A-1, B+1)
+        ans = binarySearch(seqN, low - 1, high + 1)
+        print(f"Case {casito + 1}: {ans}")
+
+
+main()
 
 
 
