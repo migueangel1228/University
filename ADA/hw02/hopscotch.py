c5bffe805976b98d4c17c40be7cf77564faa8b82
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-03-03 17:48:27 -0500

    Create hopscotch.py

---
+++ ADA/hw02/hopscotch.py
@@ -0,0 +1,30 @@
+"""
+Tarea  : ADA  Tarea 1 
+Fecha  : 1 Enero 2026
+Nombre : Miguel Angel Padilla Rosero
+Cod    : 8988878
+
+Problem D - Hippity Hopscotch
+"""
+
+from sys import stdin
+
+
+
+def main():
+    pass
+
+
+main()
+
+
+"""
+Sample Input
+1
+3 1
+1 2 5
+10 11 6
+12 12 7
+Sample Output
+37
+"""
