42e474893f693a2c71dce71a1ac2421104d5f85b
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-03-06 19:40:01 -0500

    Memoize phi; add giantyPrueb test script
    Refactor phi memoization and add a debug/test implementation. giant.py: change phiMem to use module-level mem and k, adjust signature and control flow, initialize mem and k in main, and pass a choice accumulator (A) into phiMem; also keep previous implementation commented out. giantyPrueb.py: new file with a complete memoized implementation (phi, phiMem) using a dictionary for memo and choice reconstruction, helper to reconstruct chosen roots, and a main that prints optimal cost (and the reconstruction) per test case. These changes add memoization and support for tracking optimal choices for the problem.

--- ADA/hw02/giant.py
+++ ADA/hw02/giant.py
@@ -25,40 +25,69 @@ def phi(k,l,r):
             ans = min
     return ans
 
-def phiMem(mem,k,l,r):
+mem = {}
+k = 0
+
+# def phiMem(l,r):
+#     global mem
+#     ans = 0
+#     if (l,r) in mem: 
+#         ans =  mem[(l,r)]
+#     elif l >= r:
+#         ans = 0
+#     elif r - l == 1:
+#          ans = (l + k) * 2
+#     elif r - l == 2:
+#          ans = (l + 1 + k) * 3
+#     elif r > l:
+
+#         minimo = INF
+#         for c in range(l,r + 1):
+#             aux = (phiMem(l, c - 1) + phiMem(c + 1, r) + ((c + k) * (r - l + 1)))
+#             if aux < minimo:
+#                 minimo = aux
+#             ans = minimo
+#             mem[(l,r)] = ans
+#     mem[(l,r)] = ans
+
+#     return ans
+def phiMem(A,l,r):
+    global mem
     ans = 0
     if (l,r) in mem: 
         ans =  mem[(l,r)]
     elif l >= r:
         ans = 0
     elif r - l == 1:
-         ans = (l + k) * 2
+        ans = (l + k) * 2
+
     elif r - l == 2:
-         ans = (l + 1 + k) * 3
+        ans = (l + 1 + k) * 3
     elif r > l:
 
-        min = INF
+        minimo = INF
         for c in range(l,r + 1):
-            aux = (phiMem(mem,k, l, c - 1) + phiMem(mem,k, c + 1, r) + ((c + k) * (r - l + 1)))
-            if aux < min:
-                min = aux
-            ans = min
+            aux = (phiMem(l, c - 1) + phiMem(c + 1, r) + ((c + k) * (r - l + 1)))
+            if aux < minimo:
+                minimo = aux
+                A.append(ans)
+            ans = minimo
             mem[(l,r)] = ans
     mem[(l,r)] = ans
 
     return ans
 
-
 def main():
     casitos = int(stdin.readline())
-
+    global mem, k
     for caso in range(casitos):
         
-        N, K = list(map(int,stdin.readline().split()))
-        
+        N, k = list(map(int,stdin.readline().split()))
+        mem = {}
+
         l , r = 1 , N
-        dictsito = dict()
-        opt = phiMem(dictsito,K,l,r)
+        A = []
+        opt = phiMem(A,l,r)
         print(f"Case {caso + 1}: {opt}")
 
 main()
