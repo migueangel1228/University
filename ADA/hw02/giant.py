95e6c89e9148db95090f4e41e0b2dcb6a152ee8d
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-03-07 20:50:22 -0500

    Refactor giant.py to tabular DP and move samples
    Replace recursive dict-based memoization with a 2D-list memo and a bottom-up phiTab DP to improve performance and handle large N. Adjusted phiMem to index into the 2D mem, fixed base-case expressions, and initialize mem in main; main now calls phiTab. Remove obsolete giantyPrueb.py test script. Also move giant.in/giant.out into ADA/hw02/_samples and add a sample large test case in file comments.

--- ADA/hw02/giant.py
+++ ADA/hw02/giant.py
@@ -9,60 +9,23 @@ Problem A - Best Coalitions
 
 from sys import stdin
 
+
 INF = float("inf")
 
-# k del problema | rango del problema
-def phi(k,l,r):
-    ans = 0
-    if l > r:
-        ans = 0
-    elif r > l:
-        min = INF
-        for c in range(l,r + 1):
-            aux = (phi(k, l, c - 1) + phi(k, c + 1, r) + ((c + k) * (r - l + 1)))
-            if aux < min:
-                min = aux
-            ans = min
-    return ans
-
-mem = {}
+mem = []
 k = 0
 
-# def phiMem(l,r):
-#     global mem
-#     ans = 0
-#     if (l,r) in mem: 
-#         ans =  mem[(l,r)]
-#     elif l >= r:
-#         ans = 0
-#     elif r - l == 1:
-#          ans = (l + k) * 2
-#     elif r - l == 2:
-#          ans = (l + 1 + k) * 3
-#     elif r > l:
-
-#         minimo = INF
-#         for c in range(l,r + 1):
-#             aux = (phiMem(l, c - 1) + phiMem(c + 1, r) + ((c + k) * (r - l + 1)))
-#             if aux < minimo:
-#                 minimo = aux
-#             ans = minimo
-#             mem[(l,r)] = ans
-#     mem[(l,r)] = ans
-
-#     return ans
 def phiMem(l,r):
     global mem
     ans = 0
-    if (l,r) in mem: 
-        ans =  mem[(l,r)]
+    if mem[l][r]  != -1: 
+        ans = mem[l][r]
     elif l >= r:
         ans = 0
     elif r - l == 1:
         ans = (l + k) * 2
-
     elif r - l == 2:
-        ans = (l + 1 + k) * 3
+        ans = (l + k + 1) * 3
     elif r > l:
 
         minimo = INF
@@ -71,10 +34,48 @@ def phiMem(l,r):
             if aux < minimo:
                 minimo = aux
             ans = minimo
-            mem[(l,r)] = ans
-    mem[(l,r)] = ans
+            
+    mem[l][r] = ans
 
     return ans
+
+def phiTab(l,r):
+    global mem, k
+
+    n = r   # porque en main la vas a llamar como phiTab(1, N)
+
+    # Caso base T[i][i-1] = 0   
+    # Caso base l >= r, T[i][i] = 0
+    # Caso base r - l == 1, ans = l * (r - l + 1)
+    # Caso base r - l == 2, ans = l + 1 * (r - l + 1)
+    
+    for i in range(1, n + 2):
+        mem[i][i] = 0
+        if i - 1>= 0:
+            mem[i][i - 1] = 0 
+        if i + 1 <= n:
+            mem[i][i + 1] = (i + k) * 2
+        if i + 2 <= n:
+            mem[i][i + 2] = (i + k + 1) * 3
+
+    # Llenado de izquierda a derecha de abajo a arriba(por columnas)
+    for R in range(4, n + 1):
+        for L in range(R, 0, -1):
+
+        # solo calcular casos generales
+            if R - L >= 3:
+                minimo = INF
+
+                for c in range(L, R + 1):
+                    aux = mem[L][c - 1] + mem[c + 1][R] + ((c + k) * (R - L + 1))
+                    if aux < minimo:
+                        minimo = aux
+
+                mem[L][R] = minimo
+
+    return mem[l][r]
+
+
 
 def main():
     casitos = int(stdin.readline())
@@ -82,10 +83,11 @@ def main():
     for caso in range(casitos):
         
         N, k = list(map(int,stdin.readline().split()))
-        mem = {}
-
+        mem = [[-1 for _ in range(N + 2)] for _ in range(N + 2)]
+        # mem = {}
         l , r = 1 , N
-        opt = phiMem(l,r)
+        # opt = phiMem(l,r)
+        opt = phiTab(l,r)
         print(f"Case {caso + 1}: {opt}")
 
 main()
@@ -106,4 +108,11 @@ Case 2: 6
 Case 3: 13
 Case 4: 22
 Case 5: 605
+"""
+"""
+Imput Prueba Time
+1
+600 99
+Output Prueba Time
+Case 1: 1861756
 """
