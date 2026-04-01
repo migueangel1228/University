40c46ebbc4fd09c1c89b750ac294b5986ee2ccc9
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-04-01 17:53:39 -0500

    Refactor change.py, move samples, delete poposito
    Rework Making Change solution: convert monetary values to 5-cent units, change MONEDAS representation, and add MAX_PAGO limit. Refactor cashier precompute (cambioCajero) to accept a limit and return an array, replace recursive bounded DP with a tabulated bounded knapsack (dpTabClienteTodos) and a memoized client DP helper (dpMemoCliente), and update solution to use the new structures. Improve I/O formatting (3-wide output) and cap considered payments to MAX_PAGO for performance. Also move sample input/output files into ADA/hw03/_samples and remove obsolete poposito.txt.

--- ADA/hw03/change.py
+++ ADA/hw03/change.py
@@ -10,91 +10,141 @@ Problem B - Making Change
 from sys import stdin
 
 
-INF = float('inf')
-MONEDAS = [5, 10, 20, 50, 100, 200] 
-LIMITE = 1000
+INF = float("inf")
+# monedas en formato 5 centavos, cada unidad equivale a 5 centavos
+MONEDAS = [1, 2, 4, 10, 20, 40]
+MAX_PAGO = 200
 
 
-# recibe un precio con formato "d.cc" y lo convierte a unidades de 5 centavos
+# recibe un precio dolar.centimo y lo convierte a unidades de 5 centavos
 def parsing(precio):
     entero, decimal = precio.split('.')
     centavos = int(entero) * 100 + int(decimal)
-    return centavos
+    return centavos // 5
 
 
-# tabla de cambio del cajero construida con estrategia greedy
-def cambioCajero():
-    greedy = [INF] * (LIMITE + 1)
-    greedy[0] = 0
+# tabla de cambio de cajero
+def cambioCajero(limite):
+    ans = [INF] * (limite + 1)
+    ans[0] = 0
 
-    for monto in range(1, LIMITE + 1):
+    for monto in range(1, limite + 1):
         restante = monto
         monedasUsadas = 0
 
-        for moneda in reversed(MONEDAS):
-            if restante == 0:
-                break
-
+        for indice in range(len(MONEDAS) - 1, -1, -1):
+            moneda = MONEDAS[indice]
             cantidad = restante // moneda
             monedasUsadas += cantidad
             restante -= cantidad * moneda
 
         if restante == 0:
-            greedy[monto] = monedasUsadas
+            ans[monto] = monedasUsadas
 
-    return greedy
+    return ans
 
 
-# dp acotado con memoización para las monedas reales de la billetera
-def solution(cantidades, compra, dpCambio):
-    memo = {}
-
-    def dp(indice, montoRestante):
-        if montoRestante == 0:
-            return 0
-
-        if indice == len(MONEDAS):
-            return INF
-
+# mInimo numero de monedas que el cliente necesita para juntar ese pago
+def dpMemoCliente(indice, montoRestante, cantidades, memo):
+    if montoRestante == 0:
+        ans = 0
+    elif indice == len(MONEDAS):
+        ans = INF
+    else:
         clave = (indice, montoRestante)
         if clave in memo:
-            return memo[clave]
+            ans = memo[clave]
+        else:
+            moneda = MONEDAS[indice]
+            mejor = INF
+            limite = min(cantidades[indice], montoRestante // moneda)
 
-        moneda = MONEDAS[indice]
-        mejor = INF
-        limite = min(cantidades[indice], montoRestante // moneda)
+            for usadas in range(limite + 1):
+                costo = dpMemoCliente(indice + 1, montoRestante - usadas * moneda, cantidades, memo)
+                if costo != INF:
+                    mejor = min(mejor, costo + usadas)
 
-        for usadas in range(limite + 1):
-            costo = dp(indice + 1, montoRestante - usadas * moneda)
-            if costo != INF:
-                mejor = min(mejor, costo + usadas)
+            memo[clave] = mejor
+            ans = mejor
 
-        memo[clave] = mejor
-        return mejor
+    return ans
 
+
+# def dpTabCliente(montoObjetivo, cantidades):
+#     n = len(MONEDAS)
+#     dp = [[INF] * (montoObjetivo + 1) for _ in range(n + 1)]
+#     dp[0][0] = 0
+
+#     for i in range(1, n + 1):
+#         moneda = MONEDAS[i - 1]
+#         disponibles = cantidades[i - 1]
+
+#         for monto in range(montoObjetivo + 1):
+#             mejor = dp[i - 1][monto]
+#             limite = min(disponibles, monto // moneda)
+
+#             for usadas in range(1, limite + 1):
+#                 anterior = dp[i - 1][monto - usadas * moneda]
+#                 if anterior != INF:
+#                     mejor = min(mejor, anterior + usadas)
+
+#             dp[i][monto] = mejor
+
+#     return dp[n][montoObjetivo]
+
+
+def dpTabClienteTodos(limite, cantidades):
+    dp = [INF] * (limite + 1)
+    dp[0] = 0
+
+    for i in range(len(MONEDAS)):
+        moneda = MONEDAS[i]
+        disponibles = min(cantidades[i], limite // moneda)
+
+        for _ in range(disponibles):
+            for monto in range(limite, moneda - 1, -1):
+                previo = dp[monto - moneda]
+                if previo + 1 < dp[monto]:
+                    dp[monto] = previo + 1
+
+    return dp
+
+
+def solution(cantidades, compra, arrCambio):
     ans = INF
-    for pago in range(compra, LIMITE + 1):
-        monedasCliente = dp(0, pago)
-        if monedasCliente != INF:
-            ans = min(ans, monedasCliente + dpCambio[pago - compra])
+    totalCliente = 0
+    for i in range(len(MONEDAS)):
+        totalCliente += cantidades[i] * MONEDAS[i]
 
-    return int(ans)
+    limite = min(totalCliente, MAX_PAGO)
+    dpCliente = dpTabClienteTodos(limite, cantidades)
+
+    for pago in range(compra, limite + 1):
+        monedasCliente = dpCliente[pago]
+        if monedasCliente != INF:
+            ans = min(ans, monedasCliente + arrCambio[pago - compra])
+
+    if ans == INF:
+        resultado = -1
+    else:
+        resultado = int(ans)
+
+    return resultado
 
 
 def main():
-    dpCambio = cambioCajero()
     caso = stdin.readline().strip()
+    arrCambio = cambioCajero(MAX_PAGO)
     
     while (caso != "0 0 0 0 0 0"):
         datos = caso.split()
         monedasDisponibles = list(map(int, datos[:-1]))
         valorCompra = parsing(datos[-1])
+        result = solution(monedasDisponibles, valorCompra, arrCambio)
         
-        result = solution(monedasDisponibles, valorCompra, dpCambio)
-        print(result)
+        print(f"{result:3d}")
         
         caso = stdin.readline().strip()
-
 
 main()
 
