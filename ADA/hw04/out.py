561662a482cb305842be6163f14aa36bcf87f5b3
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-04-15 13:57:25 -0500

    Add out.py with sample I/O for Problem C
    Create ADA/hw04/out.py containing a student header (name, code, date) and the Sample Input/Output for Problem C - "Out of 5". This file serves as a reference/example I/O for Homework 4.

---
+++ ADA/hw04/out.py
@@ -0,0 +1,27 @@
+"""
+Estudio : ADA 2025_2 Tarea 4 
+Fecha   : 15 Febrero 2026
+Nombre  : Miguel Angel Padilla Rosero
+Cod     : 8988878
+
+Problem C - 23 Out of 5
+""" 
+
+
+
+
+
+
+
+
+"""
+Sample Input
+1 1 1 1 1
+1 2 3 4 5
+2 3 5 7 11
+0 0 0 0 0
+Sample Output
+Impossible
+Possible
+Possible
+"""
