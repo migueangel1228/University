e03daca1d699e2bc5a10a5c407a0c7aee931b980
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-04-19 00:49:29 -0500

    Implement Out of 5 solver; add Matrioshka checker
    Add a full solution for "Out of 5" (ADA/hw04/out.py) implementing permutation-driven backtracking to test whether 5 numbers can form 23 using +, -, * operations. Add a new Matrioshka validator (Contest/ICPL/b.py) that parses sequences and uses stacks to ensure proper nesting and size constraints, printing the required messages. Also rename Contest/b.py to Contest/Round 1071 (Div. 3)/b.py (pure rename).

--- ADA/hw04/out.py
+++ ADA/hw04/out.py
@@ -7,12 +7,41 @@ Cod     : 8988878
 Problem C - 23 Out of 5
 """ 
 
+from sys import stdin
+from itertools import permutations
 
+def backtrack(nums, i, acum):
+    # Si ya usamos los 5 números, revisamos si llegamos a 23
+    if i == 5:
+        return acum == 23
 
+    # Probamos las 3 operaciones permitidas
+    return (
+        backtrack(nums, i + 1, acum + nums[i]) or
+        backtrack(nums, i + 1, acum - nums[i]) or
+        backtrack(nums, i + 1, acum * nums[i])
+    )
 
+def possible(nums):
+    # Probar todas las permutaciones
+    for perm in permutations(nums):
+        # Empezamos con el primer número de la permutación
+        if backtrack(perm, 1, perm[0]):
+            return True
+    return False
 
+def main():
+    for line in stdin:
+        nums = list(map(int, line.split()))
+        if nums == [0, 0, 0, 0, 0]:
+            break
 
+        if possible(nums):
+            print("Possible")
+        else:
+            print("Impossible")
 
+main()
 
 """
 Sample Input
