6920625ad7ffc393be276a5fba15c87ec5852db1
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-04-26 14:04:13 -0500

    Add eden.py/super.py, move samples, remove PDF
    Add a skeleton solution for the 'Garden of Eden' problem (ADA/hw04/eden.py) containing metadata, sample I/O, and stubbed backtrack/main functions. Add an empty ADA/hw04/super.py. Remove ADA/hw04/problems.pdf. Move swaps.in and swaps.out into ADA/hw04/samples/ (rename only).

---
+++ ADA/hw04/eden.py
@@ -0,0 +1,29 @@
+"""
+Estudio : ADA 2026_1 Tarea 4 
+Fecha   : 24 Abril 2026
+Nombre  : Miguel Angel Padilla Rosero
+Cod     : 8988878
+
+Problem B - Garden of Eden
+"""
+
+def backtrack():
+    pass
+
+
+def main():
+    pass
+
+
+"""
+Sample Input
+0 4 1111
+204 5 10101
+255 6 000000
+154 16 1000000000000000
+Sample Output
+GARDEN OF EDEN
+REACHABLE
+GARDEN OF EDEN
+GARDEN OF EDEN
+"""
