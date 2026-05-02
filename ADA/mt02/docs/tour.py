4c4877e97bf9c2658225b473f743da708bffd5d1
Mianparo <miguelangel1228@javerianacali.edu.co>
2026-05-02 15:41:55 -0500

    Add Tour Belt docs and Python template
    Add Problem D (Tour Belt) resources for ADA practical 2: three PDF problem statements (Help the Leaders, Robots on Ice, Tour Belt) and a Python starter file (ADA/mt02/docs/tour.py) containing author metadata, sample I/O and a placeholder main() to be implemented.

---
+++ ADA/mt02/docs/tour.py
@@ -0,0 +1,37 @@
+"""
+Estudio : ADA 2026_1 Parcial Practico 2 
+Fecha   : 2 Mayo 2026
+Nombre  : Miguel Angel Padilla Rosero
+Cod     : 8988878
+
+Problem D - Tour Belt
+"""
+
+
+def main():
+    pass
+
+main()
+
+"""
+Sample Input
+2
+4 6
+1 2 3
+2 3 2
+4 3 4
+1 4 1
+2 4 2
+1 3 2
+8 7
+1 2 2
+2 3 1
+3 4 4
+4 5 3
+5 6 4
+6 7 1
+7 8 2
+Sample Output
+8
+20
+"""
